f8194aad82e299b1a683190f6f68870c5594d6d4
Alejandro Maya <alejandromayamtz@gmail.com>
2022-04-18 21:33:34 -0500

    Agregando carpeta y la creaciÃión de un objeto

---
+++ weekly_mission_1/example1/main.js
@@ -0,0 +1,9 @@
+// 1. Creación de un objeto con propiedades
+
+let myCar = new Object(); // Creación de un objeto
+myCar.make = 'Ford'; // Guardando un valor dentro del objeto creado
+myCar.model = 'Mustang';
+myCar.year = 1969;
+
+console.log(myCar) // Imprimiendo objeto
+
