cda14b6b59fd9dc75f788ddbb1ea2f0d3a357112
Alejandro Maya <alejandromayamtz@gmail.com>
2022-04-18 21:35:27 -0500

    AA2.gregando un objeto con varibles locales y públicas

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -7,3 +7,24 @@ myCar.year = 1969;
 
 console.log(myCar) // Imprimiendo objeto
 
+// 2. Declaración de un objeto con variables locales y públicas
+
+const myModule = (() => {
+
+    // Variables de contexto local
+     const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto"
+     const privateBar = [1,2,3,4]
+     const baz = "Soy un valor que va a ser expuesto"
+    
+    // Variable para guardar las variables locales
+     const exported = {
+       publicFoo: "Valor público, pueden verme desde donde me llamen",
+       publicBar: "Otro valor público",
+           publicBaz: baz
+     }
+    
+    // Exposición de variables locales
+     return exported
+    })()
+    
+    console.log(myModule)
