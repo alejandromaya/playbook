d591bfe523b067eb5aa248f428fa81d69b47da16
Alejandro Maya <alejandromayamtz@gmail.com>
2022-04-18 23:08:23 -0500

    8. Corregimos el archivo main.js

--- weekly_mission_1/example3/main.js
+++ weekly_mission_1/example3/main.js
@@ -6,4 +6,4 @@ const logger1 = require('./logger_1')
 const logger2 = require('./logger_2')
 
 logger1('This is an informational message')
-logger2.verbose('This is a verbose message'
+logger2.verbose('This is a verbose message')
