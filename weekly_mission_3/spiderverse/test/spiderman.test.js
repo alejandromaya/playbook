b078b75f178566850268a97251e6fad6d98e819c
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-02 16:03:31 -0500

    Agregando prueba unitaria y creando clase vacia usando el método TDD

---
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -0,0 +1,11 @@
+describe("Unit Tests for Spiderman Class", () => {
+    test('1) Create an spiderman object', () => {
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        expect(andrewGarfield.name).toBe("Spiderman Sony");
+        expect(andrewGarfield.age).toBe(31);
+        expect(andrewGarfield.actor).toBe("Andrew Garfield");
+        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.studio).toBe("Sony");
+    });
+});
