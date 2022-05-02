834e917daa1fd9b4146fbea909b5c3468eaeef76
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-02 18:09:13 -0500

    Agregando clase Spiderman y su respectiva prueba

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('./../app/spiderman.js');
+
 describe("Unit Tests for Spiderman Class", () => {
     test('1) Create an spiderman object', () => {
         const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
@@ -5,7 +7,7 @@ describe("Unit Tests for Spiderman Class", () => {
         expect(andrewGarfield.name).toBe("Spiderman Sony");
         expect(andrewGarfield.age).toBe(31);
         expect(andrewGarfield.actor).toBe("Andrew Garfield");
-        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.movies).toBe(2);
         expect(andrewGarfield.studio).toBe("Sony");
     });
 });
