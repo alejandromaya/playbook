4732559e01f5b1da5aa070a149d5587ef6cae700
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-02 23:21:15 -0500

    Agregando prueba unitaria para lista usando funcion getInfo()

--- weekly_mission_3/sprint/test/services/userService.test.js
+++ weekly_mission_3/sprint/test/services/userService.test.js
@@ -9,4 +9,12 @@ describe('Test for UserService', () => {
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
     });
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1, 'AlejandroMtz', 'Alejandro');
+        const userInfoList = UserService.getInfo(user);
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe('AlejandroMtz');
+        expect(userInfoList[2]).toBe('Alejandro');
+        expect(userInfoList[3]).toBe('Sin bio');
+    });
 });
