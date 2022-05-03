1f704ebca5735b2d095f5f9b3fe5cd4cae9449aa
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-02 19:38:29 -0500

    Requerimiento 2: Las propiedades de dateCreated y lastUpdated se cambian a tipo fecha

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('./../../app/models/User');
 
 describe("Unit Tests for User class", () => {
     test('Create an User object', () => {
-        const user = new User(1, 'IsaacMaya', 'Alejandro', 'Bio', 'dateCreated', 'lastUpdated');
+        const user = new User(1, 'IsaacMaya', 'Alejandro', 'Bio');
 
         expect(user.id).toBe(1);
         expect(user.username).toBe('IsaacMaya');
         expect(user.name).toBe('Alejandro');
         expect(user.bio).toBe('Bio');
-        expect(user.dateCreated).toBe('dateCreated');
-        expect(user.lastUpdated).toBe('lastUpdated');
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     });
 });
