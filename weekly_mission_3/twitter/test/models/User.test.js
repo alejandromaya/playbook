035b629f87727fd9e3103099404a8d9da5ca32a2
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-02 19:47:01 -0500

    Agregando en la clase getters y haciendo prueba unitaria para getters

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -4,11 +4,18 @@ describe("Unit Tests for User class", () => {
     test('Create an User object', () => {
         const user = new User(1, 'IsaacMaya', 'Alejandro', 'Bio');
 
-        expect(user.id).toBe(1);
         expect(user.username).toBe('IsaacMaya');
         expect(user.name).toBe('Alejandro');
         expect(user.bio).toBe('Bio');
         expect(user.dateCreated).not.toBeUndefined();
         expect(user.lastUpdated).not.toBeUndefined();
+    });
+    test('Add getters', () =>{
+        const user = new User(1, 'IsaacMaya', 'Alejandro', 'Bio');
+
+        expect(user.getUsername).toBe('IsaacMaya');
+        expect(user.getBio).toBe('Bio');
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeUndefined();    
     });
 });
