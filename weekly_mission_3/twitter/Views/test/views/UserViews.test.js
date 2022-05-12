e973b9f4e743514ac10a9a8c5815d88ba75dabc5
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-11 21:12:08 -0500

    Agregando prueba unitario por error de paso de parametros

--- weekly_mission_3/twitter/Views/test/views/UserViews.test.js
+++ weekly_mission_3/twitter/Views/test/views/UserViews.test.js
@@ -11,4 +11,9 @@ describe("Test for UserView", () => {
         const result = UserView.createUser(payload);
         expect(result.error).toMatch(/necesitan tener un valor válido/);
     });
+    test("Return an error object whem try to create a new user with a payload with missing properties", () =>{
+       const payload = {username: "Username"};
+       const result = UserView.createUser(payload);
+       expect(result.error).toMatch(/necesitan tener un valor válido/); 
+    });    
 })
