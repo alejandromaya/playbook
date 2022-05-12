43ddb7eedcf3eb76b528c45454602763e2fce5fc
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-11 21:05:22 -0500

    Agregando una validadcion de tipos de datos y agregando su prueba unitario de la validacion

--- weekly_mission_3/twitter/Views/app/views/UserView.js
+++ weekly_mission_3/twitter/Views/app/views/UserView.js
@@ -4,6 +4,8 @@ class UserView {
     static createUser(payload){
         if(payload === null){
             return {error: "payload no existe"}
+        }else if((typeof payload.username !== 'string' || payload.usuername === null) || (typeof payload.user !== 'string' || payload.user === null) || (typeof payload.id !== 'number' || payload.id === null)){
+            return {error: "necesitan tener un valor válido"}
         }
     }
 }
--- weekly_mission_3/twitter/Views/test/views/UserViews.test.js
+++ weekly_mission_3/twitter/Views/test/views/UserViews.test.js
@@ -4,6 +4,11 @@ describe("Test for UserView", () => {
     test("Return an error object when try to create a new user with an null payload", () => {
         const payload = null;
         const result =  UserView.createUser(payload);
-        expect(result.error).toMatch(/payload no existe/)
-    })
+        expect(result.error).toMatch(/payload no existe/);
+    });
+    test("Return an error object whem try to create a new user with a payload with invalid properties", () =>{
+        const payload = {username: null, name: 12, id: "id"};
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    });
 })
